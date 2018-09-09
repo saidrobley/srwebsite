04477d5004b0bda58722674b605ab141372f06e0
Said Robley <saidrobley@gmail.com>
2018-09-09 16:34:28 -0600

    about section using grid

--- js/app.js
+++ js/app.js
@@ -3,14 +3,21 @@ let times = document.querySelector('.times');
 let menu = document.querySelector('.m-menu');
 
 hamburger.addEventListener('click', () => {
+    menu.classList.add('show');
+    hamburger.classList.add('displayNone');
+    times.classList.add('displayBlock');
+
+});
+times.addEventListener('click', () => {
+    menu.classList.remove('show');
+    hamburger.classList.remove('displayNone');
+    times.classList.remove('displayBlock');
+});
+
+/*hamburger.addEventListener('click', () => {
     times.style.display = "block";
     hamburger.style.display = "none";
     menu.style.display = "block";
-    //hamburger.classList.toggle('hide');
-    // hamburger.style.display = "none";
-    // times.classList.toggle('show');
-    // menu.classList.toggle('show');
-    //  times.className('show');
 
 });
 
@@ -18,4 +25,4 @@ times.addEventListener('click', () => {
     hamburger.style.display = "block";
     times.style.display = "none";
     menu.style.display = "none";
-});
+}); */
